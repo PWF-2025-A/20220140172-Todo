b8ebfb626c12605b596239586fb5c7012bae7116
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2025-05-02 21:31:28 +0700

    mengubah status is_admin dan memberikan feedback message

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,27 +8,45 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index()
-{
-    // $users = User::where('id', '!=', 1)->orderBy('name')->paginate(10);
-    // return view('user.index', compact('users'));
+    {
+        // $users = User::where('id', '!=', 1)->orderBy('name')->paginate(10);
+        // return view('user.index', compact('users'));
 
-    $search = request('search');
-    if ($search) {
-        $users = User::where(function ($query) use ($search) {
-            $query->where('name', 'like', '%' . $search . '%')
-                  ->orWhere('email', 'like', '%' . $search . '%');
-        })
-        ->orderBy('name')
-        ->where('id', '!=', 1)
-        ->paginate(20)
-        ->withQueryString();
-    } else {
-        $users = User::where('id', '!=', 1)
-                     ->orderBy('name')
-                     ->paginate(20);
+        $search = request('search');
+        if ($search) {
+            $users = User::where(function ($query) use ($search) {
+                $query->where('name', 'like', '%' . $search . '%')
+                    ->orWhere('email', 'like', '%' . $search . '%');
+            })
+            ->orderBy('name')
+            ->where('id', '!=', 1)
+            ->paginate(20)
+            ->withQueryString();
+        } else {
+            $users = User::where('id', '!=', 1)
+                        ->orderBy('name')
+                        ->paginate(20);
+        }
+
+        return view('user.index', compact('users'));
     }
+        public function makeadmin(User $user)
+        {
+            $user->timestamps = false;
+            $user->is_admin = true;
+            $user->save();
+            return back()->with('success', 'Make admin successfully!');
+        }
 
-    return view('user.index', compact('users'));
-}
-
+        public function removeadmin(User $user)
+    {
+            if ($user->id != 1) {
+                $user->timestamps = false;
+                $user->is_admin = false;
+                $user->save();
+                return back()->with('success', 'Remove admin successfully!');
+            } else {
+                return redirect()->route('user.index');
+            }
+    }
 }
