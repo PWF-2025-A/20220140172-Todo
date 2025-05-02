ea8232607c9f23afe074d870afe84419dbc93bef
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2025-05-02 21:50:51 +0700

    menambahkan validate dan update juga success muncul message

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -20,10 +20,36 @@ class TodoController extends Controller
     {
         return view('todo.create');
     }
-    public function edit()
-    {
-        return view('todo.edit');
+    public function edit(Todo $todo)
+{
+    if (auth()->user()->id == $todo->user_id) {
+        // dd($todo);
+        return view('todo.edit', compact('todo'));
+    } else {
+        // abort(403);
+        // abort(403, 'Not authorized');
+        return redirect()->route('todo.index')->with('danger', 'You are not authorized to edit this todo!');
     }
+}
+
+public function update(Request $request, Todo $todo)
+{
+    $request->validate([
+        'title' => 'required|max:255',
+    ]);
+
+    // Practical
+    // $todo->title = $request->title;
+    // $todo->save();
+
+    // Eloquent Way - Readable
+    $todo->update([
+        'title' => ucfirst($request->title),
+    ]);
+
+    return redirect()->route('todo.index')->with('success', 'Todo updated successfully!');
+}
+
     public function store(Request $request)
     {
         $request->validate([
