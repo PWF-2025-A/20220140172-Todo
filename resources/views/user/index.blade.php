43e8101adbc4c20cacb0300d326a673b8f9d3d8f
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2025-05-02 21:09:12 +0700

    menambahkan search data pada User

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,11 +4,59 @@
             {{ __('User') }}
         </h2>
     </x-slot>
-
     <div class="py-12">
+    <div class="px-6 text-xl text-gray-900 dark:text-gray-100">
+        <div class="flex items-center justify-between">
+            <div></div>
+            <div>
+                @if (session('success'))
+                    <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 5000)"
+                        class="pb-3 text-sm text-green-600 dark:text-green-400">{{ session('success') }}</p>
+                @endif
+                @if (session('danger'))
+                    <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 5000)"
+                        class="pb-3 text-sm text-red-600 dark:text-red-400">{{ session('danger') }}</p>
+                @endif
+            </div>
+        </div>
+    </div>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="relative overflow-x-auto">
+                    <div class="px-6 pt-6 mb-5 md:w-1/2 2xl:w-1/3">
+                        @if (request('search'))
+                            <h2 class="pb-3 text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                                Search result for : {{ request('search') }}
+                            </h2>
+                        @endif
+                        <form class="flex items-center gap-2">
+                            <div>
+                                <x-text-input id="search" name="search" type="text" class="w-50"
+                                    placeholder="Search by name or email" value="{{ request('search') }}" autofocus />
+                            </div>
+                            <div class="px-6">
+                                <x-primary-button type="submit">
+                                    {{ __('Search') }}
+                                </x-primary-button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="px-6 text-xl text-gray-900 dark:text-gray-100">
+                    <div class="flex items-center justify-between">
+                        <div></div>
+                        <div>
+                            @if (session('success'))
+                                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 5000)"
+                                    class="pb-3 text-sm text-green-600 dark:text-green-400">{{ session('success') }}</p>
+                            @endif
+                            @if (session('danger'))
+                                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 5000)"
+                                    class="pb-3 text-sm text-red-600 dark:text-red-400">{{ session('danger') }}</p>
+                            @endif
+                        </div>
+                    </div>
+                </div>
                     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
@@ -40,7 +88,6 @@
                                         </p>
                                     </td>
                                     <td class="px-6 py-4">
-                                        <!-- Action buttons can go here -->
                                     </td>
                                 </tr>
                             @empty
