5fe3bf4fe9f3d8b89050f6399d16d8f237f2636f
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2025-05-02 21:19:12 +0700

    mengganti parameter id menjadi todo

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
     Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
 
+    Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
+    Route::patch('/todo/{todo}/uncomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
+
+
     Route::get('/user', [UserController::class, 'index'])->name('user.index');
 
     Route::resource('todo', TodoController::class)->except(['show']);
